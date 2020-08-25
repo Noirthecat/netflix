e361d09febfddf9a5d4869bf0743ced28584c1a5
Tóth Máté <tothmate911@gmail.com>
2020-08-25 20:21:11 +0200

    bugfix api-gateway

--- video-service/src/main/java/com/codecool/netflix/videoservice/VideoServiceApplication.java
+++ video-service/src/main/java/com/codecool/netflix/videoservice/VideoServiceApplication.java
@@ -3,6 +3,7 @@ package com.codecool.netflix.videoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class VideoServiceApplication {
 
 	public static void main(String[] args) {
