9abfc93c35b731238f6bb95df3fe81d37c76c9f3
Tóth Máté <tothmate911@gmail.com>
2020-10-16 14:52:05 +0200

    update recommendation

--- recommendation-service/src/main/java/com/codecool/netflix/recommendationservice/controller/RecommendationController.java
+++ recommendation-service/src/main/java/com/codecool/netflix/recommendationservice/controller/RecommendationController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class RecommendationController {
 
     private RecommendationDao recommendationDao;
